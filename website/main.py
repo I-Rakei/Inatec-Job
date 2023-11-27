7f5ead136aa425725bbb705dd6ff45535a9443e8
I-Rakei <129359596+I-Rakei@users.noreply.github.com>
2023-11-27 04:19:00 +0200

    first commit

---
+++ website/main.py
@@ -0,0 +1,73 @@
+from flask import Flask, render_template, request, redirect, url_for, session
+import sqlite3
+from werkzeug.security import generate_password_hash, check_password_hash
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+# Create SQLite database and table
+conn = sqlite3.connect('user_data.db')
+cursor = conn.cursor()
+cursor.execute('''
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL,
+        password TEXT NOT NULL,
+        turma TEXT NOT NULL,
+        year INTEGER NOT NULL
+    )
+''')
+conn.commit()
+conn.close()
+
+@app.route('/')
+def home():
+    if 'username' in session:
+        return 'Hello, ' + session['username']
+    return 'You are not logged in'
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = generate_password_hash(request.form['password'])
+        turma = request.form['turma']
+        year = int(request.form['year'])
+
+        # Insert user data into the SQLite database
+        conn = sqlite3.connect('user_data.db')
+        cursor = conn.cursor()
+        cursor.execute('INSERT INTO users (username, password, turma, year) VALUES (?, ?, ?, ?)', (username, password, turma, year))
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password_candidate = request.form['password']
+
+        # Check if the user exists in the SQLite database
+        conn = sqlite3.connect('user_data.db')
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM users WHERE username = ?', (username,))
+        user = cursor.fetchone()
+        conn.close()
+
+        if user and check_password_hash(user[2], password_candidate):
+            session['username'] = username
+            return redirect(url_for('home'))
+
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
